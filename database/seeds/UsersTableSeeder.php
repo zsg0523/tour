06d77a18171bb0e6924780f953e4caf793e14c76
Eden <shenggen93@163.com>
2019-05-22 05:02:05 +0000

    用戶模型位置修改

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+        // 生成数据集合
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make();
+                      
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Eden';
+        $user->email = 'shenggen93@163.com';
+        $user->save();
+
+    }
+}
