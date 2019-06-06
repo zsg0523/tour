2189f3e13d0ca2a1b576e8a02b2c4ccf9030782a
vagrant <vagrant@homestead>
2019-06-06 09:08:56 +0000

    changeIP

--- resources/views/books/book.blade.php
+++ resources/views/books/book.blade.php
@@ -238,8 +238,8 @@
 					var equal = bookList[0].indexOf("=");
 					var parameter = bookList[0].substr(equal + 1);//问号之后的URL
 					console.log(parameter);
-//					var url = 'http://47.91.218.205/api/books/'+parameter+'?include=contents';
-                    var url = 'http://wennoanimal.test/api/books/'+parameter+'?include=contents';
+					var url = 'http://47.91.218.205/api/books/'+parameter+'?include=contents';
+                    // var url = 'http://wennoanimal.test/api/books/'+parameter+'?include=contents';
 					$.ajax({
 				        url:url,
 				        type:'GET',
