07750cb7ba968c228443c6f9913f4df097075b31
Eden <shenggen93@163.com>
2019-10-22 09:48:12 +0000

    zsg20191022 升级https

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 
 Route::group(['middleware' => ['setLocale']], function() {
 
-	Route::redirect('/', 'http://www.wennoanimal.com/web/');
+	Route::redirect('/', 'www.wennoanimal.com/web/');
 
 	Route::get('books', function () {
 	    return view('books.book'); 
