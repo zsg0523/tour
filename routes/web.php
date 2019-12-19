c6533a06afb5b5511af2e4a6a9ca74e6d35ae786
Eden <shenggen93@163.com>
2019-12-19 11:00:37 +0000

    增加域名转换

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@
 Route::group(['middleware' => ['setLocale']], function() {
 
 	Route::redirect('/', 'https://www.wennoanimal.com/web/');
+	Route::redirect('/ios', 'https://apps.apple.com/hk/app/wenno/id1071091237?l=en');
+	Route::redirect('/apk', 'https://play.google.com/store/apps/details?id=com.wennoanimal.wenno');
 	Route::redirect('/en/app', 'https://www.wennoanimal.com/web/#/features');
 	
 	Route::get('books', function () {
