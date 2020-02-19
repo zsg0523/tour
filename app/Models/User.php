885a1eee2d72ac9686fb9a27fa4af31cabaca50f
Eden <shenggen93@163.com>
2020-02-19 03:52:35 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,10 @@ Route::get('mailable', function () {
 
 Auth::routes(['verify' => true]);
 
+Route::group(['middleware' => ['auth', 'verified']], function() {
+	Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
+
 
 
 
