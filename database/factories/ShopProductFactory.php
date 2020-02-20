b94b9fb5d6b15b083754199becc5d3d1e690445e
Eden <shenggen93@163.com>
2020-02-20 03:05:56 +0000

    商品模型

---
+++ database/factories/ShopProductFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ShopProduct;
+use Faker\Generator as Faker;
+
+$factory->define(ShopProduct::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ShopProductSkuFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ShopProductSku;
+use Faker\Generator as Faker;
+
+$factory->define(ShopProductSku::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
