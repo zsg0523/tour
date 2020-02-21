58ea04a80d0ebea5493232f1ed99b1dc84ba502a
ff <772205876@qq.com>
2020-02-21 20:31:44 +0800

    ff-商城的头和底部

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,40 @@
 <footer class="footer">
-  <div class="container">
-    <p class="float-left">
-      由 <a href="https://leo108.com" target="_blank">Leo</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
-    </p>
+  	<div>
+  		<div class="listBottom">
+          	<div class="lister col-lg-10 offset-lg-1">
+              	<ul class="about list">
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
-  </div>
+	                   <a href="{{ url('/aboutUs') }}" class="titleIcon"><img src="{{asset('images/about.png')}}"></a>
+	                   	<a href="{{ url('/aboutUs') }}"><p class="text-uppercase">about us</p></a>
+	                   <li class="text-capitalize">about wenno</li>
+	                   <li class="text-capitalize">What is New?</li>
+	                   <li class="text-capitalize">Quality animal figurines</li>
+              	</ul>
+              	<ul class="contact list">
+                   		<a href="{{ url('/contact') }}" class="titleIcon"><img src="{{asset('images/contact.png')}}"></a>
+	                  	<a  href="{{ url('/contact') }}">
+	                   		<p class="text-uppercase">contact us</p>
+	                   	</a>
+	                   	<li class="text-capitalize">telephone</li>
+	                   	<li class="text-capitalize">address</li>
+	                   	<li class="text-capitalize">send emails</li>
+              	</ul>
+              	<ul class="retail list">
+                	<a  href="{{ url('/retail') }}">
+	                   <a class="titleIcon"><img src="{{asset('images/retail.png')}}"></a>
+	                   <p class="text-uppercase">retail</p>
+	                   <li class="text-capitalize">
+	                       	<img src="{{asset('images/address.png')}}">hongkong
+	                   </li>
+	                   <li class="text-capitalize">
+	                       	<img src="{{asset('images/address.png')}}">shanghai
+	                   </li>
+	                   <li class="text-capitalize">
+	                       	<img src="{{asset('images/address.png')}}">sanya
+	                   </li> 
+                	</a>
+              	</ul>
+          	</div>
+   	  	</div>
+  	</div>
 </footer>
