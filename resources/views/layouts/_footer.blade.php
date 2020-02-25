4b2ac6225e8cd356ea0c8c323463bae294b0c2a5
Flihua <772205876@qq.com>
2020-02-25 16:58:58 +0800

    ff页面修改

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,36 +5,52 @@
               	<ul class="about list">
 
 	                   <a href="{{ url('/aboutUs') }}" class="titleIcon"><img src="{{asset('images/about.png')}}"></a>
-	                   	<a href="{{ url('/aboutUs') }}"><p class="text-uppercase">about us</p></a>
-	                   <li class="text-capitalize">about wenno</li>
-	                   <li class="text-capitalize">What is New?</li>
-	                   <li class="text-capitalize">Quality animal figurines</li>
+	                   	<a href="{{ url('/aboutUs') }}"><p class="text-uppercase"><!-- about us -->关于我们</p></a>
+	                   <li class="text-capitalize"><!-- about wenno -->关于 Wenno</li>
+	                   <li class="text-capitalize"><!-- What is New? -->有新消息？</li>
+	                   <li class="text-capitalize"><!-- Quality animal figurines -->优质动物玩具</li>
               	</ul>
               	<ul class="contact list">
                    		<a href="{{ url('/contact') }}" class="titleIcon"><img src="{{asset('images/contact.png')}}"></a>
 	                  	<a  href="{{ url('/contact') }}">
-	                   		<p class="text-uppercase">contact us</p>
+	                   		<p class="text-uppercase"><!-- contact us -->联络我们</p>
 	                   	</a>
-	                   	<li class="text-capitalize">telephone</li>
-	                   	<li class="text-capitalize">address</li>
-	                   	<li class="text-capitalize">send emails</li>
+	                   	<li class="text-capitalize"><!-- telephone -->电话号码</li>
+	                   	<li class="text-capitalize"><!-- address -->地址</li>
+	                   	<li class="text-capitalize"><!-- send emails -->电子邮箱</li>
               	</ul>
               	<ul class="retail list">
                 	<a  href="{{ url('/retail') }}">
 	                   <a class="titleIcon"><img src="{{asset('images/retail.png')}}"></a>
-	                   <p class="text-uppercase">retail</p>
+	                   <p class="text-uppercase"><!-- retail -->销售地点</p>
 	                   <li class="text-capitalize">
-	                       	<img src="{{asset('images/address.png')}}">hongkong
+	                       	<img src="{{asset('images/address.png')}}">香港
 	                   </li>
-	                   <li class="text-capitalize">
+	                   <!-- <li class="text-capitalize">
 	                       	<img src="{{asset('images/address.png')}}">shanghai
 	                   </li>
 	                   <li class="text-capitalize">
 	                       	<img src="{{asset('images/address.png')}}">sanya
-	                   </li> 
+	                   </li>  -->
                 	</a>
               	</ul>
           	</div>
+   	  	</div>
+   	  	<div class="bottomAdress">
+   	  	  	<div class="copyright">
+   	  	  	 	<div class="copyrightContent">
+   	  	  	 	 	<span><a>隱私政策</a></span>
+   	  	  	 	 	<a>© 2016-2019 Wenno® 版權所有</a>
+   	  	  	 	</div>
+   	  	  	 	<div class="newsletter">
+                	<a href="https://www.facebook.com/WennoWorld/" target="_blank">
+                		<img src="{{asset('images/facebook.png')}}">
+                	</a>
+                	<a>
+                		<img src="{{asset('images/wei.png')}}">
+                	</a>
+   	  	  	 	</div>
+   	  	  	</div>
    	  	</div>
   	</div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
     <!-- Branding Image -->
-    <a class="navbar-brand " href="{{ url('/') }}">
+    <a class="navbar-brand " href="{{ url('/shop') }}">
       <img src="http://bunchparty.com/wenno/static/img/logo.6707dfc.png" style="width: 65.5px;height: 44.5px;">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
