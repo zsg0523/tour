628f51106fae0a8bb63401fbbd944b8f8aa6bb6f
Flihua <772205876@qq.com>
2020-02-25 15:03:50 +0800

    ff商品收藏页面

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -2,5 +2,5 @@
 	<li class="menu_list_user"> <a href="{{ url('/user_info') }}"><span>&nbsp;</span>账号管理</a></li>
 	<li class="menu_list_address"> <a href="{{ route('user_addresses.index') }}"><span>&nbsp;</span>收货地址</a></li>
 	<li class="menu_list_order"> <a href=""><span>&nbsp;</span>我的订单</a></li>
-	<li class="menu_list_favor"> <a href=""><span>&nbsp;</span>我的收藏</a></li>
+	<li class="menu_list_favor"> <a href="{{ route('products.favorites') }}"><span>&nbsp;</span>我的收藏</a></li>
 </ul>
--- resources/views/products/favorites.blade.php
+++ resources/views/products/favorites.blade.php
@@ -1,36 +1,61 @@
+<style type="text/css">
+  .noInfo{line-height: 100px;}
+  .noInfo a{color: #f4335e;}
+</style>
 @extends('layouts.app')
 @section('title', '我的收藏')
 
 @section('content')
 <div class="row">
-<div class="col-lg-10 offset-lg-1">
-  <div class="card">
-    <div class="card-header">我的收藏</div>
-    <div class="card-body">
-      <div class="row products-list">
-        @foreach($products as $product)
-          <div class="col-3 product-item">
-            <div class="product-content">
-              <div class="top">
-                <div class="img">
-                  <a href="{{ route('products.show', ['product' => $product->id]) }}">
-                    <img src="{{ $product->image_url }}" alt="">
-                  </a>
+    <div class="user_main disflex">
+        <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+            @include('layouts._menu')
+        </div>
+        <div class="col-xs-9 col-sm-9 col-md-10 col-lg-10">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-header">我的收藏</div>
+          <div class="card-body">
+            <div class="row products-list">
+                @if($products)
+                @foreach($products as $product)
+                  <div class="col-3 product-item">
+                    <div class="product-content">
+                      <div class="top">
+                        <div class="img">
+                          <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                            <img src="{{ $product->image_url }}" alt="">
+                          </a>
+                        </div>
+                        <div class="price"><b>￥</b>{{ $product->price }}</div>
+                        <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+                      </div>
+                      <div class="bottom">
+                        <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
+                        <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
+                      </div>
+                    </div>
+                  </div>
+                @endforeach
+              @else
+                <div class="noInfo">
+                  您还没有收藏商品，<a href="{{ url('/') }}">去逛逛></a>
                 </div>
-                <div class="price"><b>￥</b>{{ $product->price }}</div>
-                <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
-              </div>
-              <div class="bottom">
-                <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
-                <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
-              </div>
+                @endif
             </div>
+            <div class="float-right">{{ $products->render() }}</div>
           </div>
-        @endforeach
+        </div>
       </div>
-      <div class="float-right">{{ $products->render() }}</div>
     </div>
   </div>
 </div>
-</div>
+@endsection
+
+@section('scriptsAfterJs')
+<script>
+  $(document).ready(function() {
+      $('.menu_list li.menu_list_favor').addClass('active');
+    });
+</script>
 @endsection
