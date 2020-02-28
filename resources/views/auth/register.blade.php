443d8b1d1afc211e653c69b76aac311f18a4f5f0
Flihua <772205876@qq.com>
2020-02-28 16:52:42 +0800

    ff20200228-适配移动端登录注册和首页页面

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -62,7 +62,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-4 registerBtn">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
