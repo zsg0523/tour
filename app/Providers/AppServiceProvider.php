954e083a9cdc8a65f1c3de2813ff27f99289ff4d
Eden <shenggen93@163.com>
2020-03-03 02:35:36 +0000

    引入阿里云支付和微信支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\Question;
 use App\Observers\QuestionObserver;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,31 @@ class AppServiceProvider extends ServiceProvider
     {
         \API::error(function  (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException  $exception)  {
             throw new \Symfony\Component\HttpKernel\Exception\HttpException(404,  '404 Not Found');  
+        });
+
+        // 往服务容器中注入一个名为 alipay 的单例对象
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['mode']         = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个微信支付对象
+            return Pay::wechat($config);
         });
     }
 
---
+++ config/pay.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @Author: eden
+ * @Date:   2020-03-03 10:26:22
+ * @Last Modified by:   eden
+ * @Last Modified time: 2020-03-03 10:26:49
+ */
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
