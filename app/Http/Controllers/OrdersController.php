4bd83734939d94a45b2a03f80c3536bbea472242
Eden <shenggen93@163.com>
2020-03-13 07:17:21 +0000

    评论列表，提交评论逻辑

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,8 @@ use App\Models\UserAddress;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
 use App\Exceptions\InvalidRequestException;
+use Carbon\Carbon;
+use App\Http\Requests\SendReviewRequest;
 
 class OrdersController extends Controller
 {
@@ -54,6 +56,67 @@ class OrdersController extends Controller
         // 返回原页面
         return $order;
     }
+
+    /** [review 订单评价] */
+    public function review(Order $order)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+
+        // 判断订单是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+
+        // 使用load 方法加载数据，避免n+1问题
+        return view('orders.review', ['order' => $order->load(['items.shopProductSku', 'items.shopProduct'])]);
+
+    }
+
+    /** [sendReview 提交评价] */
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+
+        // 判断是否已经评价
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+
+        $reviews = $request->input('reviews');
+
+        \DB::transaction(function () use ($reviews, $order) {
+            // 遍历用户提交的数据
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                // 保存评分和评价
+                $orderItem->update([
+                    'rating'      => $review['rating'],
+                    'review'      => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                ]);
+            }
+            // 将订单标记为已评价
+            $order->update(['reviewed' => true]);
+        });
+
+        return redirect()->back();
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
