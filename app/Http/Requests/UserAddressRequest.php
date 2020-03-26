b5639f92ef588593d0e668a723f5ef285c6c4358
Eden <shenggen93@163.com>
2020-03-26 09:22:34 +0000

    修改地址编辑

--- app/Http/Requests/UserAddressRequest.php
+++ app/Http/Requests/UserAddressRequest.php
@@ -13,11 +13,11 @@ class UserAddressRequest extends Request
     public function rules()
     {
         return [
-            'province'      => 'required',
-            'city'          => 'required',
-            'district'      => 'required',
+            // 'province'      => 'required',
+            // 'city'          => 'required',
+            // 'district'      => 'required',
             'address'       => 'required',
-            'zip'           => 'required',
+            // 'zip'           => 'required',
             'contact_name'  => 'required',
             'contact_phone' => 'required',
         ];
@@ -26,11 +26,11 @@ class UserAddressRequest extends Request
     public function attributes()
     {
         return [
-            'province'      => '省',
-            'city'          => '城市',
-            'district'      => '地区',
+            // 'province'      => '省',
+            // 'city'          => '城市',
+            // 'district'      => '地区',
             'address'       => '详细地址',
-            'zip'           => '邮编',
+            // 'zip'           => '邮编',
             'contact_name'  => '姓名',
             'contact_phone' => '电话',
         ];
