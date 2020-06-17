a3f06eded00cb17d544f6f15654fa312b352fb69
Eden <shenggen93@163.com>
2020-06-17 08:16:39 +0000

    迁移文件修改

--- database/migrations/2020_01_07_110849_add_total_to_questions_table.php
+++ database/migrations/2020_01_07_110849_add_total_to_questions_table.php
@@ -14,7 +14,7 @@ class AddTotalToQuestionsTable extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->integer('total')->after('false');
+            $table->integer('total')->after('falses');
         });
     }
 
