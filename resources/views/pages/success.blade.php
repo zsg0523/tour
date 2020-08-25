1690cd0ec6be3e53f5d788d54d2e9f5a6e158b73
Eden <shenggen93@163.com>
2020-08-25 09:13:19 +0000

    取消订阅后提示

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app') -->
 @section('title', '操作成功')
 
 @section('content')
@@ -6,7 +6,7 @@
     <div class="card-header">{{ __('shop.page.success') }}</div>
     <div class="card-body text-center">
       <h1>{{ $msg }}</h1>
-      <a class="btn btn-primary" href="{{ route('root') }}">{{ __('shop.page.gohome') }}</a>
+      <!-- <a class="btn btn-primary" href="{{ route('root') }}">{{ __('shop.page.gohome') }}</a> -->
     </div>
   </div>
 @endsection
